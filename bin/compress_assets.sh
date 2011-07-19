84617589c3658ff8866d7312e5a99eb974f44ec7
Jake Archibald <jakearchibald@theteam.co.uk>
2011-07-19 08:07:59 +0100

    Updating jquery

--- bin/compress_assets.sh
+++ bin/compress_assets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ver=3
-cat www/assets/$ver/script/{jquery-1.6.1,jquery.transition,jquery.clickjack,intro,MicroEvent,Rect,ImgInput,SpriteCanvas,SpriteCanvasView,CssOutput,Toolbar,pageLayout,FeatureTest,featureTests,base}.js | bin/uglify/bin/uglifyjs --unsafe -o www/assets/$ver/script/mainmin.js
+cat www/assets/$ver/script/{jquery-1.6.2,jquery.transition,jquery.clickjack,intro,MicroEvent,Rect,ImgInput,SpriteCanvas,SpriteCanvasView,CssOutput,Toolbar,pageLayout,FeatureTest,featureTests,base}.js | bin/uglify/bin/uglifyjs --unsafe -o www/assets/$ver/script/mainmin.js
 cat www/assets/$ver/style/base.css | java -jar bin/yuicompressor-2.4.5.jar --type css -o www/assets/$ver/style/mainmin.css
