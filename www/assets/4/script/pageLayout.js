58d63daedaa6289a5a89fe7b1c108a26257841e4
Jake Archibald <jakearchibald@theteam.co.uk>
2011-08-25 08:24:41 +0100

    Removing padding from calculation

--- www/assets/4/script/pageLayout.js
+++ www/assets/4/script/pageLayout.js
@@ -13,7 +13,7 @@ spriteCow.pageLayout = (function() {
 		currentView = 'intro';
 	
 	function getContainerWidthPercent() {
-		var bodyHorizontalPadding = 60,
+		var bodyHorizontalPadding = 0,
 			containerRelativeWidth = $container.width() / ( $window.width() - bodyHorizontalPadding );
 		
 		return Math.round(containerRelativeWidth * 10000) / 100 + '%';
