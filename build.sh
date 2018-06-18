eab0a11e388d92942263d8667972b073d4e5523a
Jake Archibald <jaffathecake@gmail.com>
2018-06-18 14:27:48 +0100

    Adding caching headers

--- build.sh
+++ build.sh
@@ -14,5 +14,6 @@ cp -r www/assets/style/fonts build/assets/$ver/style/
 cp -r www/assets/style/imgs build/assets/$ver/style/
 sed -e 's/\/$ver\//\/'"$ver"'\//g' www/index.html > build/index.html
 sed -e 's/\/$ver\//\/'"$ver"'\//g' www/offline.appcache > build/offline.appcache
+sed -e 's/\/$ver\//\/'"$ver"'\//g' www/_headers > build/_headers
 cat www/assets/script/jquery-1.7.1.js www/assets/script/jquery.easing.js www/assets/script/jquery.transition.js www/assets/script/jquery.fileClickjack.js www/assets/script/intro.js www/assets/script/MicroEvent.js www/assets/script/Rect.js www/assets/script/ImgInput.js www/assets/script/SpriteCanvas.js www/assets/script/SpriteCanvasView.js www/assets/script/InlineEdit.js www/assets/script/CssOutput.js www/assets/script/Toolbar.js www/assets/script/pageLayout.js www/assets/script/FeatureTest.js www/assets/script/featureTests.js www/assets/script/base.js | uglifyjs -cm > build/assets/$ver/script/mainmin.js
 node-sass --output-style compessed www/assets/style/all.scss build/assets/$ver/style/all-min.css
