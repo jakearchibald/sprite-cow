49ac732786b47eb67135970e7919510169ab53b9
Jake Archibald <jaffathecake@gmail.com>
2018-06-18 09:13:57 +0100

    Updating build

--- bin/compress_assets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ver=8
-cat www/assets/$ver/script/{jquery-1.7.1,jquery.easing,jquery.transition,jquery.fileClickjack,intro,MicroEvent,Rect,ImgInput,SpriteCanvas,SpriteCanvasView,InlineEdit,CssOutput,Toolbar,pageLayout,FeatureTest,featureTests,base}.js | node bin/uglify.js > www/assets/$ver/script/mainmin.js
-~/dev/sass/bin/sass --style compressed www/assets/$ver/style/all.scss:www/assets/$ver/style/all-min.css
---
+++ build.sh
@@ -0,0 +1,16 @@
+ver=9
+
+rm -r build
+
+mkdir build
+mkdir build/assets
+mkdir build/assets/$ver
+mkdir build/assets/$ver/script
+mkdir build/assets/$ver/style
+
+cp www/assets/{favicon.ico,tutorial-sprite.png} build/assets/$ver/
+cp -r www/assets/style/{fonts,imgs} build/assets/$ver/style/
+sed -e 's/\/$ver\//\/'"$ver"'\//g' www/index.html > build/index.html
+sed -e 's/\/$ver\//\/'"$ver"'\//g' www/offline.appcache > build/offline.appcache
+cat www/assets/script/{jquery-1.7.1,jquery.easing,jquery.transition,jquery.fileClickjack,intro,MicroEvent,Rect,ImgInput,SpriteCanvas,SpriteCanvasView,InlineEdit,CssOutput,Toolbar,pageLayout,FeatureTest,featureTests,base}.js | uglifyjs -cm > build/assets/$ver/script/mainmin.js
+node-sass --output-style compessed www/assets/style/all.scss build/assets/$ver/style/all-min.css
